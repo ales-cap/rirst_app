506b4896977b6baf060f0e3be15b873824a3c3fc
ales_cap <a.a.vlasenko@yandex.ru>
2013-06-05 22:46:13 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RirstApp::Application.config.secret_token = 'cc9e6950342135363683556883bbed8e30050fd83babe617b3d0b2941492bb8fb94988ef30a68a3812fe61cbfedf2fb47271f7e5e89a75ef57768eaa3c64eb9d'
